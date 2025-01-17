9ecd2ecc2188510daa4d420f6ad904853f193f87
marat <maratyaku@gmail.com>
2025-01-17 17:10:44 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+def generate_math_question(a, b):
+    num1 = random.randint(a, b)
+    operator = random.choice(["/", "*", "+", "-"])
+    num2 = random.randint(a, b)
+    question = f"{num1} {operator} {num2}"
+    return question
+def check_answer(question,user_answer):
+    try:
+        user_answer = float(user_answer)
+        return user_answer == eval(question)
+    except ValueError:
+        return False
+def math_quiz(number_of_questions = 5):
+    print("Добро пожаловать в математический тест!")
+    correct_answers = 0
+    for i in range(number_of_questions):
+        question = generate_math_question(1, 5)
+        user_answer = input(f"{question} = ")
+        if check_answer(question, user_answer):
+            correct_answers += 1
+
+    print("Тест завершен.")
+    print(f"Вы правильно решили {correct_answers} из {number_of_questions} вопросов.")
+
+    if correct_answers >= number_of_questions * 0.8:
+        print("Отлично! Вы получаете оценку A.")
+    elif correct_answers >= number_of_questions * 0.6:
+        print("Хорошо! Вы получаете оценку B.")
+    else:
+        print("Попробуйте еще раз. Вы получаете оценку C.")
+
+
+# Запуск теста
+math_quiz(7)
